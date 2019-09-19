2b37006ef539470596582c672f6af405b506971c
Bert Sultanov <sultanovalbertrash@gmail.com>
2019-09-19 18:21:46 +0300

    Changed db schema

--- postgres/initdb.d/0.schema.sql
+++ postgres/initdb.d/0.schema.sql
@@ -11,24 +11,24 @@ create table localization.word
 );
 create table localization.translate
 (
-    uuid uuid primary key  not null default uuid_generate_v4(),
-    word     varchar(256) not null references localization.word(name) on delete cascade on update cascade,
+    id uuid primary key  not null default uuid_generate_v4(),
+    word varchar(256) not null references localization.word(name) on delete cascade on update cascade,
     language char(2) not null references localization.language(code) on delete cascade on update cascade,
-    value    text
+    value text
 );
 
 create schema card;
 create table card.class 
 (
-    uuid uuid primary key  not null default uuid_generate_v4(),
+    id varchar(128) primary key  not null,
     name varchar(256) not null references localization.word(name) on delete restrict on update cascade
 );
-create table card.entity 
+create table card.instance 
 (
-    uuid uuid primary key  not null default uuid_generate_v4(),
+    id uuid primary key  not null default uuid_generate_v4(),
     created timestamp with time zone not null default current_timestamp,
-    class uuid references card.class(uuid) default null,
-    parent uuid references card.entity(uuid) default null,
+    class varchar(128) default null references card.class(id) on delete restrict on update cascade,
+    parent uuid default null references card.instance(id) on delete restrict on update cascade,
     name varchar(256) not null references localization.word(name) on delete restrict on update cascade,
     description varchar(256) default null references localization.word(name) on delete restrict on update cascade,
     image text default null,
@@ -37,40 +37,40 @@ create table card.entity
 );
 create table card.binder 
 (
-    uuid uuid primary key  not null default uuid_generate_v4(),
-    entity uuid references card.entity(uuid) not null,
-    input uuid references card.entity(uuid) not null,
-    output uuid references card.entity(uuid) default null
+    parent uuid references card.instance(id) not null,
+    child uuid references card.instance(id) not null,
+    next uuid references card.instance(id) default null,
+    primary key(parent, child, next)
 );
 
 create schema deck;
-create table deck.entity
+create table deck.instance
 (
-    uuid uuid primary key  not null default uuid_generate_v4(),
+    id uuid primary key  not null default uuid_generate_v4(),
     created timestamp with time zone not null default current_timestamp,
     name varchar(256) default null references localization.word(name) on delete restrict on update cascade
 );
 create table deck.content
 (
-    deck uuid not null references deck.entity(uuid) on delete cascade on update cascade,
-    card uuid not null references card.entity(uuid) on delete cascade on update cascade,
+    deck uuid not null references deck.instance(id) on delete cascade on update cascade,
+    card uuid not null references card.instance(id) on delete cascade on update cascade,
     position int not null,
     primary key(deck, card)
 );
 
 create schema booster;
-create table booster.entity
+create table booster.instance
 (
-    uuid uuid primary key  not null default uuid_generate_v4(),
+    id uuid primary key  not null default uuid_generate_v4(),
     created timestamp with time zone not null default current_timestamp,
     name varchar(256) default null references localization.word(name) on delete restrict on update cascade,
     charge smallint not null check (charge > 0)
 );
 create table booster.content
 (
-    uuid       uuid primary key not null default uuid_generate_v4(),
-    booster uuid not null references booster.entity(uuid) on delete cascade on update cascade,
-    card uuid not null references card.entity(uuid) on delete cascade on update cascade,
+    id uuid primary key not null default uuid_generate_v4(),
+    booster uuid not null references booster.instance(id) on delete cascade on update cascade,
+    card uuid not null references card.instance(id) on delete cascade on update cascade,
     chance numeric(6,6) check(chance > 0.0 and chance <= 1.0),
     unique(booster, card)
 );
@@ -78,18 +78,18 @@ create table booster.content
 create schema account;
 create table account.user
 (
-    uuid       uuid primary key              not null default uuid_generate_v4(),
-    created    timestamp with time zone not null default current_timestamp,
-    name       varchar(256)                      default null,
-    avatar     text default null,
-    deleted    timestamp with time zone default null
+    id uuid primary key              not null default uuid_generate_v4(),
+    created timestamp with time zone not null default current_timestamp,
+    name varchar(256)                      default null,
+    avatar text default null,
+    deleted timestamp with time zone default null
 );
 create table account.email 
 (
-    uuid     uuid primary key            not null default uuid_generate_v4(),
-    address  varchar(256) not null,
+    id uuid primary key            not null default uuid_generate_v4(),
+    address varchar(256) not null,
     created timestamp with time zone not null default current_timestamp,     
-    owner    uuid              not null references account.user(uuid) on delete cascade on update cascade
+    owner uuid              not null references account.user(id) on delete cascade on update cascade
 );
 
 create type account.method as enum('facebook', 'internal');
@@ -98,37 +98,37 @@ create table account.sign
     method account.method not null,
     key text not null,
     created timestamp with time zone not null default current_timestamp,
-    owner uuid default null references account.user(uuid) on delete cascade on update cascade,
+    owner uuid default null references account.user(id) on delete cascade on update cascade,
     primary key(method, key)
 );
 create table account.token
 (
-    uuid       uuid primary key not null default uuid_generate_v4(),
+    id uuid primary key not null default uuid_generate_v4(),
     created timestamp with time zone not null default current_timestamp,
     updated timestamp with time zone not null default current_timestamp,
     deleted timestamp with time zone default null,
-    owner    uuid not null references account.user(uuid) on delete cascade on update cascade,
+    owner    uuid not null references account.user(id) on delete cascade on update cascade,
     ip       inet default null
 );
 create table account.card
 (
-    uuid       uuid primary key not null default uuid_generate_v4(),
-    entity uuid not null references card.entity(uuid)  on delete cascade on update cascade,
-    owner uuid not null references account.user(uuid) on delete cascade on update cascade,
+    id uuid primary key not null default uuid_generate_v4(),
+    instance uuid not null references card.instance(id)  on delete cascade on update cascade,
+    owner uuid not null references account.user(id) on delete cascade on update cascade,
     created timestamp with time zone not null default current_timestamp
 );
 create table account.deck
 (
-    uuid       uuid primary key not null default uuid_generate_v4(),
-    entity uuid not null references deck.entity(uuid)  on delete cascade on update cascade,
-    owner uuid not null references account.user(uuid)  on delete cascade on update cascade,
+    id uuid primary key not null default uuid_generate_v4(),
+    instance uuid not null references deck.instance(id)  on delete cascade on update cascade,
+    owner uuid not null references account.user(id)  on delete cascade on update cascade,
     created timestamp with time zone not null default current_timestamp,
     name text
 );
 create table account.booster
 (
-    uuid       uuid primary key not null default uuid_generate_v4(),
-    entity uuid not null references booster.entity(uuid)  on delete cascade on update cascade,
-    owner uuid not null references account.user(uuid)  on delete cascade on update cascade,
+    id uuid primary key not null default uuid_generate_v4(),
+    instance uuid not null references booster.instance(id)  on delete cascade on update cascade,
+    owner uuid not null references account.user(id)  on delete cascade on update cascade,
     created timestamp with time zone not null default current_timestamp
 );
--- postgres/initdb.d/1.data.sql
+++ postgres/initdb.d/1.data.sql
@@ -2,3 +2,34 @@ INSERT INTO localization.language VALUES
 ('ru'), 
 ('en')
 ;
+-- INSERT INTO localization.word
+-- VALUES
+-- ('offense'),
+-- ('defense')
+-- ;
+-- INSERT INTO localization.translate
+-- (word,language, value)
+-- VALUES
+-- ('card.class.offense', 'ru', 'Атака'),
+-- ('card.class.offense', 'en', 'Offense'),
+-- ('card.class.defense', 'ru', 'Защита'),
+-- ('card.class.defense', 'en', 'Defense'),
+-- ('card.class.partial', 'ru', 'Компонент'),
+-- ('card.class.partial', 'en', 'Component'),
+-- ;
+
+
+-- INSERT INTO card.class
+-- (id, name)
+-- VALUES
+-- ('offense', 'card.class.offense')
+-- ('defense', 'card.class.defense')
+-- ('partial', 'card.class.partial')
+-- ;
+
+-- INSERT INTO card.entity 
+-- (id,class,parent,name) 
+-- VALUES 
+-- ('7a1ec838-2a51-4bc5-a251-a45b7fc4c4b1', 'offense', NULL, 'A1')
+-- ('7a1ec838-2a51-4bc5-a251-a45b7fc4c4b1', 'offense', NULL, 'A1')
+-- ;
