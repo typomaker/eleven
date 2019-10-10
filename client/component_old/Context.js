a54b7640307f6eb3cb7443b362449601a6a379df
Bert Sultanov <sultanovalbertrash@gmail.com>
2019-10-11 02:17:38 +0300

    Removed directory with legacy components

--- client/component_old/Context.js
+++
@@ -1,5 +0,0 @@
-import {createContext} from "react";
-
-export const Language = createContext(navigator.language);
-export const Token = createContext('');
-export const GoogleMapToken = createContext('AIzaSyBUJwo1ytf4wOMKQK4kW8b-rh9hadFghfY');
--- client/component_old/Map.jsx
+++
@@ -1,9 +0,0 @@
-import React, {Suspense} from 'react';
-import {withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
-
-
-class Map extends React.Component {
-    render() {
-
-    }
-}
