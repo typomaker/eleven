2c7fec303ec2040205fd23f0d3dd853650b6b0b6
Bert Sultanov <sultanovalbertrash@gmail.com>
2020-02-27 02:58:41 +0300

    JSON request body types changed

--- mainframe/src/application/Server.ts
+++ mainframe/src/application/Server.ts
@@ -7,12 +7,12 @@ import Container from "./Container";
 type Message = (
   | Message.Failure
   | Message.Success
-  | Message.Signin
-  | Message.Signout
-  | Message.Authorize
   | Message.Token
+  | Message.Token.Create
+  | Message.Token.Delete
+  | Message.Token.Get
   | Message.Account
-  | Message.Account.GET
+  | Message.Account.Get
 )
 namespace Message {
   export type Failure = { type: "Failure", payload: string }
@@ -31,45 +31,46 @@ namespace Message {
     return { type: "Success" }
   }
 
-  export type Signin = { type: "Signin", payload: { type: "password" | "facebook", data: string, email: string } }
-  export function Signin(payload: Signin["payload"]): Signin {
-    return { type: "Signin", payload }
-  }
-
-  export namespace Signin {
-    export function is(target: any): target is Signin {
-      return (
-        target?.type === "Signin"
-        && validator.isIn(target?.payload?.type ?? "", ["password", "facebook",])
-        && !validator.isEmpty(target?.payload?.data ?? "")
-        && validator.isEmail(target?.payload?.email ?? "")
-      )
-    }
-  }
-
-  export type Signout = { type: "Signout", payload: { token: string } }
-  export function Signout(payload: Signout["payload"]): Signout {
-    return { type: "Signout", payload }
-  }
-
-  export namespace Signout {
-    export function is(target: Signout | any): target is Signout {
-      return !validator.isEmpty(target?.token)
-    }
-  }
-  export type Authorize = { type: "Authorize", payload: { token: string } }
-  export function Authorize(payload: Authorize["payload"]): Authorize {
-    return { type: "Authorize", payload }
-  }
-  export namespace Authorize {
-    export function is(target: Authorize | any): target is Authorize {
-      return !validator.isEmpty(target?.token)
-    }
-  }
-
   export type Token = { type: "Token", payload: { id: string, expired?: Date } }
-  export const Token = (token: entity.Token): Token => {
+  export function Token(token: entity.Token): Token {
     return { type: "Token", payload: { id: token.id, expired: token.expired ?? undefined } }
+  }
+  export namespace Token {
+    export type Create = { type: "Token:create", payload: { type: "password" | "facebook", data: string, email: string } }
+    export function Create(payload: Create["payload"]): Create {
+      return { type: "Token:create", payload }
+    }
+    export namespace Create {
+      export function is(target: any): target is Create {
+        if (target?.type !== "Token:create") return false
+        if (!validator.isIn(target?.payload?.type ?? "", ["password", "facebook",])) return false
+        if (validator.isEmpty(target?.payload?.data ?? "")) return false;
+        if (!validator.isEmail(target?.payload?.email ?? "")) return false;
+        return true
+      }
+    }
+    export type Delete = { type: "Token:delete", payload: { token: string } }
+    export function Delete(payload: Delete["payload"]): Delete {
+      return { type: "Token:delete", payload }
+    }
+    export namespace Delete {
+      export function is(target: Delete | any): target is Delete {
+        if (target?.type !== "Token:delete") return false
+        if (validator.isEmpty(target?.token)) return false
+        return true;
+      }
+    }
+    export type Get = { type: "Token:get", payload: { token: string } }
+    export function Get(payload: Get["payload"]): Get {
+      return { type: "Token:get", payload }
+    }
+    export namespace Get {
+      export function is(target: Get | any): target is Get {
+        if (target?.type !== "Token:get") return false
+        if (validator.isEmpty(target?.token)) return false
+        return true;
+      }
+    }
   }
 
   export type Account = { type: "Account", payload: Pick<entity.Account, "id" | "avatar" | "name"> }
@@ -78,9 +79,9 @@ namespace Message {
   }
 
   export namespace Account {
-    export type GET = { type: "Account.GET", payload: { id: string, token: string } }
-    export const GET = (payload: GET["payload"]): GET => {
-      return { type: "Account.GET", payload }
+    export type Get = { type: "Account:get", payload: { id: string, token: string } }
+    export function Get(payload: Get["payload"]): Get {
+      return { type: "Account:get", payload }
     }
   }
 }
@@ -105,16 +106,16 @@ class Server {
     let res: Message = Message.Failure.Unexpected;
     try {
       switch (req.type) {
-        case "Signin": {
-          res = Message.Signin.is(req) ? Message.Token(await this.container.account.signin(req.payload)) : Message.Failure.Invalid;
+        case "Token:create": {
+          res = Message.Token.Create.is(req) ? Message.Token(await this.container.account.signin(req.payload)) : Message.Failure.Invalid;
           break;
         }
-        case "Signout": {
-          res = Message.Signout.is(req) ? Message.Token(await this.container.account.signout(req.payload.token)) : Message.Failure.Invalid;
+        case "Token:delete": {
+          res = Message.Token.Delete.is(req) ? Message.Token(await this.container.account.signout(req.payload.token)) : Message.Failure.Invalid;
           break;
         }
-        case "Authorize": {
-          res = Message.Authorize.is(req) ? Message.Token(await this.container.account.authorize(req.payload.token)) : Message.Failure.Invalid;
+        case "Token:get": {
+          res = Message.Token.Get.is(req) ? Message.Token(await this.container.account.authorize(req.payload.token)) : Message.Failure.Invalid;
           break;
         }
       }
@@ -122,7 +123,7 @@ class Server {
       if (e instanceof Account.Error) {
         res = Message.Failure(e.message)
       } else {
-        this.container.log.error(e)
+        this.container.log.error(e, e.stack)
         res = Message.Failure.Internal;
       }
     }
