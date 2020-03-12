6da4820aafa1a4ebb5bd792e3d6be490f5bdac2d
Bert Sultanov <sultanovalbertrash@gmail.com>
2020-03-12 21:08:39 +0300

    Code refactoring

--- server/src/application/Container.ts
+++ server/src/application/Container.ts
@@ -8,21 +8,21 @@ import WSocket from "./WSocket";
 class Container {
   constructor(private readonly config: Container.Configuration) { }
 
-  #log?: Logger
-  public get log() {
-    if (this.#log) return this.#log;
-    return this.#log = new Logger();
+  #logger?: Logger;
+  public get logger() {
+    if (this.#logger) return this.#logger;
+    return this.#logger = new Logger();
   }
   #password?: Password;
   public get password() {
     if (this.#password) return this.#password;
-    return this.#password = new Password(this.config.password)
+    return this.#password = new Password(this.config.password);
   }
   #pg?: pg.Pool;
   public get pg() {
     if (this.#pg) return this.#pg;
     return this.#pg = new pg.Pool(this.config.pg)
-      .on("error", (err) => this.log.error("Unexpected error on idle client", err));
+      .on("error", (err) => this.logger.error("Unexpected error on idle client", err));
   }
   #storage?: Storage;
   public get storage() {
@@ -39,15 +39,15 @@ class Container {
     if (this.#recaptcha2) return this.#recaptcha2;
     return this.#recaptcha2 = new Recaptcha2(this.config.recaptcha2);
   }
-  #wsocket?: WSocket
+  #wsocket?: WSocket;
   public get wsocket() {
     if (this.#wsocket) return this.#wsocket;
     return this.#wsocket = new WSocket(this);
   }
 }
 namespace Container {
-  export type Configuration = {
-    password: Password.Configuration,
+  export interface Configuration {
+    password: Password.Configuration;
     pg: pg.PoolConfig;
     recaptcha2: Recaptcha2.Options;
   }
