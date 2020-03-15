960cfcf97adf8ce6c81e61a7f4fa34d74b564f58
Bert Sultanov <sultanovalbertrash@gmail.com>
2020-03-16 02:55:27 +0300

    Include all new components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,12 @@
 import * as React from 'react';
 import CssBaseline from "@material-ui/core/CssBaseline";
-import Signin from './Signin';
 import Theme from './Theme';
 import Configuration from "./Configuration"
 import WSocket from "./WSocket";
+import Session from "./Session";
+import Localization from "./Localization";
+import Splashscreen from "./Splashscreen";
+import Router from "./Router";
 
 export default class App extends React.Component {
     public render(): React.ReactNode {
@@ -11,11 +14,19 @@ export default class App extends React.Component {
             <>
                 <Theme>
                     <CssBaseline />
-                    <Configuration.Provider>
-                        <WSocket.Provider>
-                            <Signin />
-                        </WSocket.Provider>
-                    </Configuration.Provider>
+                    <Localization.Provider>
+                        <Configuration.Provider>
+                            <WSocket.Provider>
+                                <Session.Provider>
+                                    <Session.Account.Provider>
+                                        <Splashscreen>
+                                            <Router />
+                                        </Splashscreen>
+                                    </Session.Account.Provider>
+                                </Session.Provider>
+                            </WSocket.Provider>
+                        </Configuration.Provider>
+                    </Localization.Provider>
                 </Theme>
             </>
         );
